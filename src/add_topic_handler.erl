10d00aff12bdfe90b7a08171f109b71dd5809285
Mateusz Piotrowski <m.piotrowski@campus.tu-berlin.de>
2019-04-29 21:50:07 +0200

    Silence some compiler warnings
    ```
    $ make
     DEPEND msq_ctf_service.d
     ERLC   add_topic_handler.erl msq_ctf_service_app.erl msq_ctf_service_sup.erl publish_handler.erl socket_server.erl subscribe_handler.erl topics_handler.erl
    compile: warnings being treated as errors
    src/add_topic_handler.erl:2: undefined callback function terminate/3 (behaviour 'cowboy_handler')
    erlang.mk:5335: recipe for target 'ebin/msq_ctf_service.app' failed
    make[1]: *** [ebin/msq_ctf_service.app] Error 1
    erlang.mk:5082: recipe for target 'app' failed
    make: *** [app] Error 2
    ```

--- src/add_topic_handler.erl
+++ src/add_topic_handler.erl
@@ -2,6 +2,7 @@
 -behavior(cowboy_handler).
 
 -export([init/2]).
+-export([code_change/3, handle_call/3, handle_info/2, terminate/3]).
 
 init(Req0, State) ->
 	Req = cowboy_req:reply(200,
@@ -9,3 +10,9 @@ init(Req0, State) ->
         <<"ADD TOPIC!">>,
         Req0),
     {ok, Req, State}.
+
+% XXX: Silence compiler warnings.
+handle_call(_Msg, _Caller, State) -> {noreply, State}.
+handle_info(_Msg, Library) -> {noreply, Library}.
+terminate(_Reason, _Library, _State) -> ok.
+code_change(_OldVersion, Library, _Extra) -> {ok, Library}.
