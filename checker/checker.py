07c6ddd3be335f4db76f9f22949570784b324635
Mateusz Piotrowski <m.piotrowski@campus.tu-berlin.de>
2019-05-15 16:42:08 +0200

    Remove template implementation of getflag

--- checker/checker.py
+++ checker/checker.py
@@ -60,58 +60,7 @@ class MessageQueueChecker(BaseChecker):
         self.debug("Flag put ({})".format(self.flag))
 
     def getflag(self):
-        self.http_get("/")
-
-        try:
-            tag = self.team_db[self.flag]
-        except KeyError as ex:
-            raise BrokenServiceException(
-                "Inconsistent Database: Couldn't get tag for team/flag ({})".format(
-                    self.flag
-                )
-            )
-
-        r = self.http_get("/api/SearchAttacks", params={"needle": tag})
-        self.info("Parsing search result")
-        try:
-            search_results = json.loads(r.text)
-            id = search_results["matches"][0]["id"]
-        except Exception as ex:
-            raise BrokenServiceException(
-                "Invalid JSON Response: {} ({})".format(r.text, ex)
-            )
-
-        self.info("Found attack (id={})".format(id))
-        self.info("Fetching attack: {}".format({"id": id, "password": self.flag}))
-
-        r = self.http_get(
-            "/api/GetAttack",
-            params={"id": id, "password": self.flag},
-            timeout=5,
-            verify=False,
-        )
-        self.info("Parsing GetAttack result")
-        try:
-            attack_results = json.loads(r.text)
-        except Exception:
-            raise BrokenServiceException("Invalid JSON: {}".format(r.text))
-
-        try:
-            flag_field = "attackDate" if self.flag_idx % 2 == 0 else "location"
-            if attack_results["attack"][flag_field] != self.flag:
-                raise BrokenServiceException(
-                    "Incorrect flag in date field (searched for {} in {} - {})".format(
-                        self.flag, attack_results, flag_field
-                    )
-                )
-        except Exception as ex:
-            if isinstance(ex, BrokenServiceException):
-                raise
-            raise BrokenServiceException(
-                "Error parsing json: {}. {} (expected: {})".format(
-                    attack_results, ex, self.flag
-                )
-            )
+        pass
 
     def putnoise(self):
         pass
