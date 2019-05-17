341559d2dee64b81a3659c0f00409e07e872e85c
Mateusz Piotrowski <m.piotrowski@campus.tu-berlin.de>
2019-05-17 23:40:02 +0200

    Replace global macros with @property

--- checker/checker.py
+++ checker/checker.py
@@ -11,47 +11,59 @@ from enochecker import BaseChecker, BrokenServiceException, run, sha256ify
 session = requests.Session()
 
 
-# /publish
-# * POST
-# * Headers -- Content-type: application/x-www-form-urlencoded
-# * Payload: `Topic 1:message_string`
-# * Sends call to event handler `file_handler` to save received msg to file
-#   `Topic 1_msg_save.txt`
-PUBLISH_ENDPOINT = "/publish"
-# /subscribe
-# * Upgrades to Websocket automatically
-# * When connected use `SUBSCRIBE: topicname` to subscribe to topics
-# * Use `REPLAY: topicname` to receive all messages sent to Topic `topicname`
-SUBSCRIBE_ENDPOINT = "/subscribe"
-# /topics
-# * GET
-TOPICS_ENDPOINT = "/topics"
-# `/add_topic`
-# * PATCH
-# * Headers -- Content-type: application/x-www-form-urlencdoed
-# * Payload:
-#     * `- topicname` for private topics
-#     * `+ topicname` or `topicname` for public topics
-# * Creates file `topicname_msg_save.txt`. The file is used to store
-#   messages sent to this topic
-ADD_TOPIC_ENDPOINT = "/add_topic"
-
-SUBSCRIBE_ENDPOINT_GREETING = "Heyhey from WS Handler.."
-
-
 class MessageQueueChecker(BaseChecker):
     port = 8080  # default port to send requests to.
+
+    @property
+    def greeting(self):
+        return "Heyhey from WS Handler.."
+
+    # /publish
+    # * POST
+    # * Headers -- Content-type: application/x-www-form-urlencoded
+    # * Payload: `Topic 1:message_string`
+    # * Sends call to event handler `file_handler` to save received msg to file
+    #   `Topic 1_msg_save.txt`
+    @property
+    def publish_endpoint(self):
+        return "/publish"
+
+    # /subscribe
+    # * Upgrades to Websocket automatically
+    # * When connected use `SUBSCRIBE: topicname` to subscribe to topics
+    # * Use `REPLAY: topicname` to receive all messages sent to Topic `topicname`
+    @property
+    def subscribe_endpoint(self):
+        return "/subscribe"
+
+    # /topics
+    # * GET
+    @property
+    def topics_endpoint(self):
+        return "/topics"
+
+    # `/add_topic`
+    # * PATCH
+    # * Headers -- Content-type: application/x-www-form-urlencdoed
+    # * Payload:
+    #     * `- topicname` for private topics
+    #     * `+ topicname` or `topicname` for public topics
+    # * Creates file `topicname_msg_save.txt`. The file is used to store
+    #   messages sent to this topic
+    @property
+    def add_topic_endpoint(self):
+        return "/add_topic"
 
     # Send a replay request for the given topic to the subscribe endpoint.
     def replay(self, topic: str) -> str:
         async def request(topic: str):
             async with websockets.connect(
-                f"ws://{self.address}:{self.port}{SUBSCRIBE_ENDPOINT}"
+                f"ws://{self.address}:{self.port}{self.subscribe_endpoint}"
             ) as websocket:
                 # Ignore the greeting message.
                 greeting = await websocket.recv()
 
-                if greeting != SUBSCRIBE_ENDPOINT_GREETING:
+                if greeting != self.greeting:
                     raise BrokenServiceException(
                         f'Broken service: the subscribe endpoint greeted us with "{greeting}"'
                     )
@@ -65,7 +77,7 @@ class MessageQueueChecker(BaseChecker):
         return asyncio.get_event_loop().run_until_complete(request(topic))
 
     def add_private_topic(self, topic):
-        return self.http("PATCH", ADD_TOPIC_ENDPOINT, data="- {topic}")
+        return self.http("PATCH", self.add_topic_endpoint, data="- {topic}")
 
     def putflag(self):
         topic = sha256ify(self.flag)
