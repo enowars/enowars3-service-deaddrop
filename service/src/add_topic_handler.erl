a1f70993ea4f2645468a1a41c4d5a5ff73af7eea
Otto Bittner <otto-bittner@gmx.de>
2019-05-19 11:28:44 +0200

    Restructure add_topic_handler to include topic size check

--- service/src/add_topic_handler.erl
+++ service/src/add_topic_handler.erl
@@ -6,34 +6,38 @@
 
 init(Req0=#{method := <<"PATCH">>}, State) ->
     % Decode http body
-    NewTopic = case cowboy_req:read_body(Req0) of
-        {ok, Data, _} -> 
-            string:trim(binary_to_list(Data));
+    Req = case cowboy_req:read_body(Req0) of
+        {ok, Data, _} when erlang:size(Data) =< 1000 -> 
+            handle_valid_req(Req0, string:trim(binary_to_list(Data)));
+        {ok, Data, _} when erlang:size(Data) >= 1000 ->
+            cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0);
         {error, _} -> io:fwrite("Error while parsing sent topic")
     end,
-
-    Req = case string:find(NewTopic, "\n") of 
-        nomatch -> 
-            case check_duplicate_topic(NewTopic) of 
-                {ok, false} -> 
-                    create_message_save(NewTopic),
-                    gen_event:call({global, file_handler}, file_handler, {new_topic, NewTopic}),
-                    cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>},<<"">>,Req0);
-                % {ok, true} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Topic already exists."],Req0);
-                {ok, true} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0);
-                {error, _} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0)
-                % {error, ErrMsg} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},[io:format("Error: ~p \n", [ErrMsg])],Req0)
-            end;
-        _ -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0)
-    end,
     {ok, Req, State};
-
 
 init(Req0, State) ->
     Req = cowboy_req:reply(405, #{
         <<"allow">> => <<"PATCH">>
     }, Req0),
     {ok, Req, State}.
+
+handle_valid_req(Req0, Topic) ->
+    case string:find(Topic, "\n") of 
+        nomatch -> 
+            case check_duplicate_topic(Topic) of 
+                {ok, false} -> 
+                    create_message_save(Topic),
+                    gen_event:call({global, file_handler}, file_handler, {new_topic, Topic}),
+                    cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>},<<"">>,Req0);
+                % {ok, true} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Topic already exists."],Req0);
+                {ok, true} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0);
+                {error, _} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0)
+                % {error, ErrMsg} -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},[io:format("Error: ~p \n", [ErrMsg])],Req0)
+            end;
+
+        _ -> cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>},["Bad Request."],Req0)
+    end.
+
 
 % Create file to save messages published to this topic
 create_message_save(Topic) ->
