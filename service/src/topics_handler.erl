05542d80b2ba36390044ac7908eb8800ac49e9dc
Otto Bittner <otto-bittner@gmx.de>
2019-05-19 12:15:16 +0200

    Modify status code for empty topics file in topics_handler

--- service/src/topics_handler.erl
+++ service/src/topics_handler.erl
@@ -7,7 +7,7 @@ init(Req0=#{method := <<"GET">>}, State) ->
     Topics = gen_event:call({global, file_handler}, file_handler, {topics}),
     Req = case catch hd(Topics) of 
         {'EXIT', {badarg, _}} -> 
-            cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>}, ["Bad Request."], Req0);
+            cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, [""], Req0);
         _ -> 
             CleanedTopics = lists:map(fun remove_private_topics/1, Topics),
             cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, [CleanedTopics], Req0)
